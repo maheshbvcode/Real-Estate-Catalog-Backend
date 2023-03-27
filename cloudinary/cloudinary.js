c6de83d5e8fd43fe3f499a8c90171cd51ec45431
Mahesh Bv <maheshbv2000@gmail.com>
2023-03-27 08:58:38 +0530

    Added Post Endpoint

--- cloudinary/cloudinary.js
+++
@@ -1,9 +0,0 @@
-const cloudinary = require("cloudinary");
-require("dotenv").config();
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API,
-  api_secret: process.env.CLOUD_SECRET,
-});
-
-module.exports = cloudinary;
--- routes/properties.js
+++ routes/properties.js
@@ -1,1 +1,57 @@
 const router = require("express").Router();
+const property = require('../models/propertySchema');
+const user = require('../models/userSchema');
+const { generateToken, validateToken } = require("../middleware/token");
+
+
+router.post('/properties/add',validateToken, async (req, res) => {
+    // console.log(req.body)
+    try {
+        const lastProp = await property.find().sort({ _id: -1 }).limit(1);
+           //console.log(value);
+            let ppd_id = "PPD";
+            //console.log(value.length);
+            if (lastProp.length !== 0) {
+                ppd_id = parseInt(lastProp[0].ppdId.split("D")[1]) + 1;
+                // console.log(ppd_id)
+            } else {
+                ppd_id =1100;
+                // console.log(ppd_id)
+            }
+            const views = parseInt(Math.random() * 100);
+            const dayLeft = parseInt(Math.random() * 30);
+            
+            const {propertyType,length,breadth,contact} = req.body;
+            const area = parseInt(req.body.length) * parseInt(req.body.breadth) ; 
+            const newProperty = await property.create({
+                ppdId: "PPD"+ppd_id,
+                views: views,
+                status: "Unsold",
+                daysLeft: dayLeft,
+                propertyType,
+                length,
+                breadth,
+                totalArea: area,
+                contact,
+                ...req.body
+
+            });
+
+            res.status(200).json({
+                newProperty
+            })
+    }
+    catch(err) {
+        console.error(err);
+        res.status(500).json({
+            message : err.message
+        })
+
+    }
+})
+
+
+
+
+
+module.exports = router;
